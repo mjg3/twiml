d4ccb0b025a32bc4e30248f492a4507d79b6b8cd
mjg3 <mjg3@princeton.edu>
2018-03-24 21:56:13 -0500

    changed formatting to match examples

--- python_twilio.py
+++ python_twilio.py
@@ -14,6 +14,9 @@ call = client.calls.create(
     url="https://twiml-xml.herokuapp.com/index.xml"
 )
 
-
+response = VoiceResponse()
 gather = Gather(input='speech dtmf', timeout=3, num_digits=1)
+gather.say('Please press 1 or say sales for sales.')
+response.append(gather)
+
 print(response)
