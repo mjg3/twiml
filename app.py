58a9453992c8592e3c814b838154a46c10912e86
mjg3 <mjg3@princeton.edu>
2018-03-25 14:13:30 -0500

    Hardcoded the return URL

--- app.py
+++ app.py
@@ -10,24 +10,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'POST':
-        print request.form
-    else:
-        account_sid = "AC17aff2a74960a87c99ce7037342c1982"
-        # Your Auth Token from twilio.com/console
-        auth_token  = "2900ed6c5c3dff153f90fee834d37a82"
+    account_sid = "AC17aff2a74960a87c99ce7037342c1982"
+    # Your Auth Token from twilio.com/console
+    auth_token  = "2900ed6c5c3dff153f90fee834d37a82"
 
-        client = Client(account_sid, auth_token)
-        #
-        print "About to call"
-        call = client.calls.create(
-            to="+19737382206",
-            from_="+18026130730",
-            url="https://twiml-xml.herokuapp.com/index.xml"
-        )
+    client = Client(account_sid, auth_token)
+    #
+    print "About to call"
+    call = client.calls.create(
+        to="+19737382206",
+        from_="+18026130730",
+        url="https://twiml-xml.herokuapp.com/index.xml"
+    )
 
-# def collect():
-
+    return "Main"
+    
+@app.route('/handle_data', methods=['GET', 'POST'])
+def handle_data():
+    return "Handle Data"
     # response = VoiceResponse()
     # gather = Gather(input='speech dtmf', timeout=3, num_digits=1)
     # gather.say('Please press 1 or say sales for sales.')
@@ -36,9 +36,6 @@ def main():
     # print response
 
     # return 'Welcome'
-
-def collection():
-    print "collecting"
 
 if __name__ == "__main__":
     app.run()
